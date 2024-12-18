de99ce429d24e752c1c8e5158ec29d2fe3b38e75
sun-xm <sun-xm@hotmail.com>
2024-12-18 17:31:55 +0800

    Update GLBuffer.h

--- Source/GLBuffer.h
+++ Source/GLBuffer.h
@@ -7,7 +7,7 @@ class GLBuffer
 public:
     GLBuffer(GLenum target = GL_ARRAY_BUFFER) : buffer(0), target(target) {}
     GLBuffer(const GLBuffer& other) = delete;
-    virtual ~GLBuffer() {}
+    virtual ~GLBuffer() { this->Release(); }
 
     bool Create()
     {
